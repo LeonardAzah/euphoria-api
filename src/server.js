ae9b39429b20bdf05191442c6fb9dd0fb70c9750
LeonardAzah <isongleonard@gmail.com>
2024-04-14 23:07:16 +0100

    validation checks

--- src/server.js
+++ src/server.js
@@ -90,29 +90,3 @@ const start = async () => {
 };
 
 start();
-
-// const exitHandler = () => {
-//   if (server) {
-//     server.close(() => {
-//       logger.info("Server closed");
-//       process.exit(1);
-//     });
-//   } else {
-//     process.exit(1);
-//   }
-// };
-
-// const unexpectedErrorHandler = (error) => {
-//   logger.error(error);
-//   exitHandler();
-// };
-
-// process.on("uncaughtException", unexpectedErrorHandler);
-// process.on("unhandledRejection", unexpectedErrorHandler);
-
-// process.on("SIGTERM", () => {
-//   logger.info("SIGTERM received");
-//   if (server) {
-//     server.close();
-//   }
-// });
